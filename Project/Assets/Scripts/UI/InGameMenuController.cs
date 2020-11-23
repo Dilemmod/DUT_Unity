68a40c59a9aff3e4847c7a868d7da9e33208842f
Dileon <yevtayev@gmail.com>
2020-11-24 01:32:32 +0200

    Build_Ver.1

---
+++ Project/Assets/Scripts/UI/InGameMenuController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameMenuController : BaseGameMenuController
+{
+    [Header("Buttons")]
+    [SerializeField] private Button restart;
+    [SerializeField] private Button backToMenu;
+
+    protected override void Start()
+    {
+        base.Start();
+        play.onClick.AddListener(OnChangeMenuStatusClicked);
+        restart.onClick.AddListener(levelManager.Restart);
+        backToMenu.onClick.AddListener(OnGoToMainMenuClicked);
+
+    }
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        play.onClick.RemoveListener(OnChangeMenuStatusClicked);
+        restart.onClick.RemoveListener(levelManager.Restart);
+        backToMenu.onClick.RemoveListener(OnGoToMainMenuClicked);
+    }
+    protected override void OnChangeMenuStatusClicked()
+    {
+        base.OnChangeMenuStatusClicked();
+        Time.timeScale =(menu.activeInHierarchy ? 0 : 1);
+    }
+    public void OnGoToMainMenuClicked()
+    {
+        LevelManager.Instance.ChangeLvl((int)Scenes.MainMenu);
+    }
+}
---
+++ Project/Assets/Scripts/UI/SettingsMenuController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenuController : MonoBehaviour
+{
+    [Header("Volume")]
+    [SerializeField] private Slider volume;
+    [SerializeField] private AudioMixer masterMixer;
+    private float curretVolume;
+
+    [Space]
+    [SerializeField] private Toggle fullScreen;
+    [Space]
+    [SerializeField] Dropdown resolutionDropdown;
+    private Resolution[] availableResolutins;
+    [Space]
+    [SerializeField] Dropdown qualityDropdown;
+    private string[] qualityLevels;
+
+    void Start()
+    {
+        volume.onValueChanged.AddListener(OnVolumeChanged);
+        fullScreen.onValueChanged.AddListener(OnFullScreenChanged);
+        resolutionDropdown.onValueChanged.AddListener(OnResolutionChanged);
+        qualityDropdown.onValueChanged.AddListener(OnQualityChanged);
+
+        masterMixer.GetFloat("Volume", out curretVolume);
+        volume.value = curretVolume;
+
+        availableResolutins = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentIndex = 0;
+        for (int i = 0; i < availableResolutins.Length; i++) 
+        {
+            if (availableResolutins[i].width <= 800)
+                continue;
+
+            options.Add(availableResolutins[i].width + "x" + availableResolutins[i].height);
+            if (availableResolutins[i].width == Screen.currentResolution.width
+                && availableResolutins[i].height == Screen.currentResolution.height)
+                currentIndex = i;
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        qualityLevels = QualitySettings.names;
+        qualityDropdown.ClearOptions();
+        qualityDropdown.AddOptions(qualityLevels.ToList());
+        int qulityLvl = QualitySettings.GetQualityLevel();
+        qualityDropdown.value = qulityLvl;
+        qualityDropdown.RefreshShownValue();
+
+    }
+    private void OnDestroy()
+    {
+        volume.onValueChanged.RemoveListener(OnVolumeChanged);
+        fullScreen.onValueChanged.RemoveListener(OnFullScreenChanged);
+        resolutionDropdown.onValueChanged.RemoveListener(OnResolutionChanged);
+        qualityDropdown.onValueChanged.RemoveListener(OnQualityChanged);
+
+    }
+    void Update()
+    {
+        
+    }
+    private void OnResolutionChanged(int resolutionIndex)
+    {
+        Resolution resolution = availableResolutins[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    private void OnFullScreenChanged(bool value)
+    {
+        Screen.fullScreen = value;
+    }
+    private void OnVolumeChanged(float volume)
+    {
+        masterMixer.SetFloat("Volume", volume);
+    }
+    private void OnQualityChanged(int qualityLvl)
+    {
+        QualitySettings.SetQualityLevel(qualityLvl,true);
+    }
+}
